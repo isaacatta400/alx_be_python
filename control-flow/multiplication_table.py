9006f8083c125db321716ce70d8bc1d96b81ec17
Isaac Atta Nyarkoh <isaacattanyarkoh6@gmail.com>
2025-06-21 23:07:12 +0000

    Added a python file

--- control-flow/multiplication_table.py
+++ control-flow/multiplication_table.py
@@ -12,4 +12,4 @@ number = int(input("Enter a number to see its multiplication table:"))
 
 for iterator in range(1, 11):
   product = iterator * number
-  print(number, "*", iterator, "=", product)
+  print(f"{number} * {iterator} = {product}")
